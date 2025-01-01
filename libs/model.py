c93e1a59521161a08dfd497f25c61beb0d542ce2
Xinxiang <angelowxx@gmail.com>
2025-01-01 07:07:23 +0100

    initialization

--- libs/model.py
+++ libs/model.py
@@ -5,11 +5,11 @@ class Semantic_Segmentater(nn.Module):
     def __init__(self, channels=3, class_num=40):
         super(Semantic_Segmentater, self).__init__()
         self.model = nn.Sequential(
-            nn.Conv2d(in_channels=channels, out_channels=64, kernel_size=9, stride=1, padding=4, padding_mode='reflect'),
+            nn.Conv2d(in_channels=channels, out_channels=40, kernel_size=9, stride=1, padding=4, padding_mode='reflect'),
             nn.MaxPool2d(3, stride=2),
             nn.LeakyReLU(),
             nn.Upsample(scale_factor=2, mode='bilinear', align_corners=False),
-            nn.Conv2d(in_channels=class_num, out_channels=class_num, kernel_size=9, stride=1, padding=4,
+            nn.Conv2d(in_channels=40, out_channels=class_num, kernel_size=9, stride=1, padding=4,
                       padding_mode='reflect'),
             nn.Softmax(dim=-3),
         )
