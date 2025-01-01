13560b28d1cf7ea805f9ca18601f5569b4ba3e2e
Xinxiang <angelowxx@gmail.com>
2025-01-01 07:02:14 +0100

    initialization

--- libs/model.py
+++ libs/model.py
@@ -8,7 +8,7 @@ class Semantic_Segmentater(nn.Module):
             nn.Conv2d(in_channels=channels, out_channels=64, kernel_size=9, stride=1, padding=4, padding_mode='reflect'),
             nn.MaxPool2d(3, stride=2),
             nn.LeakyReLU(),
-            nn.Conv2d(in_channels=64, out_channels=class_num, kernel_size=9, stride=1, padding=4, padding_mode='reflect'),
+            nn.Conv2d(in_channels=64, out_channels=class_num, kernel_size=5, stride=1, padding=2, padding_mode='reflect'),
             nn.MaxPool2d(3, stride=2),
             nn.LeakyReLU(),
             nn.Upsample(scale_factor=2, mode='bilinear', align_corners=False),
